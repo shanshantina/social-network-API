bf9f5f34e5022ac471c52ba29c66568f1b311223
Shanshan Xu <tinaxu84@gmail.com>
2021-03-30 14:05:32 -0400

    created models

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            validate: {
+                validator: function(email) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(email);
+                },
+                message: props => `${props.value} is not a valid email address, please entry the valid email address as 'example1@test.com'`
+            },
+            required: [true, 'User email address required']    
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
